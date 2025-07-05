eda054ed96b676336aced674710f68499268edd0
Logan Nelson <Lnelson0915@gmail.com>
2025-07-05 10:39:45 -0700

    Input validation

--- WinFormsSourceControl/Form1.cs
+++ WinFormsSourceControl/Form1.cs
@@ -1,10 +1,31 @@
-namespace WinFormsSourceControl
-{
+namespace WinFormsSourceControl;
+
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void CalcAreaAndPerimeterButton_Click(object sender, EventArgs e)
+        {
+            if (!int.TryParse(lengthTextBox.Text, out int length))
+            {
+                MessageBox.Show("Please enter a valid integer for length.", "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (!int.TryParse(widthTextBox.Text, out int width))
+            {
+                MessageBox.Show("Please enter a valid integer for width.", "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            int area = length * width;
+            int perimeter = length * 2 + width * 2;
+
+            areaTextbox.Text = "Area is: " + area;
+            perimeterTextBox.Text = "Perimeter is: " + perimeter;
+        }
     }
-}
+
